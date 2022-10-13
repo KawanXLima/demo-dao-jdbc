d7bd0ff93d5d227b0650aeb472a9a92c0a9516cc
KawanXLima <kawan.ximendes@gmail.com>
2022-10-13 20:31:08 -0300

    segundo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ import model.entities.Department;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department dtp = new Department(1, "Books");
+		System.out.println(dtp);
 	}
 }
